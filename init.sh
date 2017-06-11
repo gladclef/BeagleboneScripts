bbe83c0f68cbf04b848757bbe37a12e795c131e3
Benjamin Bean <benjamin.bean@student.nmt.edu>
2017-06-11 21:31:38 +0000

    added check to prevent scripts from running if in an SFTP session

--- init.sh
+++ init.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# DON'T run scripts if in an SFTP session, since executing
+# these scripts produces terminal output and that breaks the
+# SFTP protocol.
+if [[ -z "$SSH_TTY" ]]
+then
+	exit
+fi
+
 # initializes environment variables PINS and SLOTS
 initEnviroVars.sh
 
